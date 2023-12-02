8eca57a363495acfb2e818e85453276f03ef8bcc
siya <mzam.siya@gmail.com>
2023-12-02 16:09:26 +0200

    Add payment finalized.

--- frontend/src/components/FinalizePayment.tsx
+++ frontend/src/components/FinalizePayment.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useMemo } from 'react';
+import { Heading, Spinner } from '@chakra-ui/react';
+import { useEffect, useMemo, useState } from 'react';
 import { useLocation } from 'react-router';
 
 function useQuery() {
@@ -14,25 +15,37 @@ const finalizePaymentRequest = async (params: any): Promise<any> => {
 
 export const FinalizePayment = () => {
   const query: any = useQuery();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const params = {
-    orderNumber: query.state,
-    code: query.code,
-  };
   useEffect(() => {
-    finalizePaymentRequest(params);
-  });
+    const params = {
+      orderNumber: query.state,
+      code: query.code,
+    };
 
-  // finalizePaymentRequest(params)
-  //     .then((data) => {
-  //         res.send(mapSuccessResponse(data, 200));
-  //     })
-  //     .catch((error: CustomError) => {
-  //         return res.status(error.statusCode).send(mapFailureResponse(error));
-  //     });
-  return (
+    finalizePaymentRequest(params);
+  }, [query.code, query.state]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, [isLoading]);
+
+  return isLoading ? (
     <div>
       <h1>Finalizing Payment</h1>
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </div>
+  ) : (
+    <div>
+      <Heading>Payment Successful!</Heading>
     </div>
   );
 };
